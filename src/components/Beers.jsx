cafee68e931c8df12be1f10c27dbd4f155d00ece
HenriqueCG81 <123783235+HenriqueCG81@users.noreply.github.com>
2023-06-19 16:20:28 +0100

    done

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -4,21 +4,48 @@ import { NavLink } from 'react-router-dom';
 
 const Beers = () => {
   const [beers, setBeers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    axios
-      .get('https://ih-beers-api2.herokuapp.com/beers')
-      .then(response => {
-        setBeers(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching beers:', error);
-      });
+    fetchBeers();
   }, []);
+
+  const fetchBeers = async () => {
+    try {
+      const response = await axios.get(
+        'https://ih-beers-api2.herokuapp.com/beers'
+      );
+      setBeers(response.data);
+    } catch (error) {
+      console.error('Error fetching beers:', error);
+    }
+  };
+
+  const handleSearch = async event => {
+    const { value } = event.target;
+    setSearchQuery(value);
+
+    try {
+      const response = await axios.get(
+        `https://ih-beers-api2.herokuapp.com/beers/search?q=${value}`
+      );
+      setBeers(response.data);
+    } catch (error) {
+      console.error('Error searching for beers:', error);
+    }
+  };
 
   return (
     <div className="container">
       <h1 className="mt-5">Beers</h1>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleSearch}
+        placeholder="Search for beers..."
+        className="form-control mb-4"
+      />
+
       {beers.map(beer => (
         <div key={beer._id} className="row mb-4">
           <div className="col-4">
